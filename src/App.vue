3ea952c699a50a8493027e3aa0a5ef7a9da9794b
Danny <dannywo@gmail.com>
2023-11-24 21:59:47 -0800

    Adds static content for components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
-<template></template>
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+</script>
+
+<template>
+    <Header></Header>
+    <div class="container">
+        <Balance />
+        <IncomeExpenses />
+    </div>
+</template>
