2881c5a6d8ab9464a6f7e0201455728f24d9246f
Danny <dannywo@gmail.com>
2023-11-26 13:34:19 -0800

    Adds transaction iteration and total computed

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,24 @@ import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
+
+import { ref } from "vue";
+import { computed } from "@vue/reactivity";
+
+const transactions = ref([
+    { id: 1, text: "Flowers", amount: -19.99 },
+    { id: 2, text: "Bi-Weekly Paycheck", amount: 2919.99 },
+    { id: 3, text: "Mortgage", amount: -1730.25 },
+    { id: 3, text: "Nutflix", amount: -9.99 },
+]);
+
+const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+        return acc + transaction.amount;
+    }, 0);
+});
+
+console.log(total.value);
 </script>
 
 <template>
@@ -11,7 +29,7 @@ import AddTransaction from "./components/AddTransaction.vue";
     <div class="container">
         <Balance />
         <IncomeExpenses />
-        <TransactionList />
+        <TransactionList :transactions="transactions" />
         <AddTransaction />
     </div>
 </template>
