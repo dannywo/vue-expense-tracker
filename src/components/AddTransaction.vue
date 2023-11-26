2bc9cca640d1ed825908803a88f29c6f7b69d22b
Danny <dannywo@gmail.com>
2023-11-25 19:04:31 -0800

    Updates content/markup

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,9 @@
+<template>
+    <h3></h3>
+    <form action="" id="form">
+        <div class="form-control">
+            <label for="text"></label>
+            <input type="text" id="text" />
+        </div>
+    </form>
+</template>
