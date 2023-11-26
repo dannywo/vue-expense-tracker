80837a82424de880c271403c2b7f592bc1ff6592
Danny <dannywo@gmail.com>
2023-11-25 19:16:52 -0800

    Completes markup

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -2,8 +2,16 @@
     <h3></h3>
     <form action="" id="form">
         <div class="form-control">
-            <label for="text"></label>
-            <input type="text" id="text" />
+            <label for="text">Text</label>
+            <input type="text" id="text" placeholder="Enter text..." />
         </div>
+        <div class="form-control">
+            <label for="amount"
+                >Amount<br />
+                (negative - expense, positive - income)</label
+            >
+            <input type="number" id="amount" placeholder="Enter amount..." />
+        </div>
+        <button class="btn">Add transaction</button>
     </form>
 </template>
