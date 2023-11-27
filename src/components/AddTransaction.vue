8f5f34d18dd0db728ffc376076826ce46214947c
Danny <dannywo@gmail.com>
2023-11-26 19:27:17 -0800

    attaches model for inputs, work pending.

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,17 +1,40 @@
 <template>
     <h3></h3>
-    <form action="" id="form">
+    <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..." />
+            <input
+                type="text"
+                id="text"
+                v-model="text"
+                placeholder="Enter text..."
+            />
         </div>
         <div class="form-control">
             <label for="amount"
                 >Amount<br />
                 (negative - expense, positive - income)</label
             >
-            <input type="number" id="amount" placeholder="Enter amount..." />
+            <input
+                type="text"
+                id="amount"
+                v-model="amount"
+                placeholder="Enter amount..."
+            />
         </div>
         <button class="btn">Add transaction</button>
     </form>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const text = ref("");
+const amount = ref("");
+
+const onSubmit = () => {
+    console.log("submitted.");
+    console.log(text.value);
+    console.log(amount.value);
+};
+</script>
